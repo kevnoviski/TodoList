eb6c5b07f0737507d48dd9a1900fac5ffaa0e34b
Kevin Noviski <Oem@OEM-PC>
2021-02-28 19:19:56 -0300

    added a comment

--- Dtos/TodoItemReadDto.cs
+++ Dtos/TodoItemReadDto.cs
@@ -2,11 +2,12 @@ using System;
 
 namespace TodoList.Dtos
 {
-    public class TodoItemReadDto
+    public class TodoItemReadDto//contains all the original fields, its just here for study purpose
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public DateTime DT_Creation { get; set; }
         public DateTime DT_DeadLine { get; set; }
         public DateTime? DT_Done { get; set; }
         public int Priority { get; set; }
